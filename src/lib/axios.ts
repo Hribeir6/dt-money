3ea73a96960baabcd2cb13f86ac6554357c8ec92
Henrique <hribeir6@gmail.com>
2024-12-05 11:48:51 -0300

    updated

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,14 +4,3 @@ export const api = axios.create({
   baseURL: 'http://localhost:3000',
  })
 
-
-
-
-//  const url = new URL('/transactions');
-
-//       if (query) { 
-//         url.searchParams.append('q', query);
-//       }
-
-//       const response = await fetch(url)
-//       const data = await response.json();
